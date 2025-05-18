0be11efd48a51ca0d4be670c31cd9fb17403df12
Sunday Stanley <sundaystanley56@gmail.com>
2025-05-18 17:44:05 +0100

    Add update

--- app/(pages)/verify-payment/page.tsx
+++ app/(pages)/verify-payment/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Button from '@/app/components/button/Button';
 import { useVerifyPayment } from '@/app/features/bootcamp/bootcampService';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { BsPatchCheckFill } from 'react-icons/bs';
 import { FadeLoader } from 'react-spinners';
@@ -9,6 +9,7 @@ import { PiSealWarningFill } from 'react-icons/pi';
 
 const VerifyPayment = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const txtref = searchParams.get('txtref');
   const reference = searchParams.get('reference');
   const { isLoading, isError } = useVerifyPayment({ txtref: txtref as string, reference: reference as string });
@@ -30,17 +31,35 @@ const VerifyPayment = () => {
       <div className="flex flex-col items-center justify-center h-screen bg-[#111111] text-white w-full gap-4">
         <PiSealWarningFill className="text-red-500 text-[50px]" />
         <p className="text-lg font-bold text-red-500">Payment verification failed</p>
-        <p className="text-sm text-gray-500 mb-5">Please try again later or contact support.</p>
+        <p className="text-sm text-gray-500">Please try again later or contact support.</p>
+        <Button
+          label="Back to home"
+          width="w-fit"
+          onClick={() => router.push('/')}
+          buttonStyle="custom"
+          height="h-[45px]"
+          labelSize="text-[15px] font-lg rounded-md"
+          customClasses="cursor-pointer text-white bg-[#684DF4] px-2 rounded-md transition-transform duration-300 mt-5 ease-in-out hover:scale-110 hover:opacity-90"
+        />
       </div>
     );
   }
-  
+
   if (!txtref || !reference) {
     return (
       <div className="flex flex-col items-center justify-center h-screen bg-[#111111] text-white w-full gap-4">
         <PiSealWarningFill className="text-red-500 text-[50px]" />
         <p className="text-lg font-bold text-red-500">Invalid payment details</p>
-        <p className="text-sm text-gray-500 mb-5">Please check your payment link and try again.</p>
+        <p className="text-sm text-gray-500">Please check your payment link and try again.</p>
+        <Button
+          label="Back to home"
+          width="w-fit"
+          onClick={() => router.push('/')}
+          buttonStyle="custom"
+          height="h-[45px]"
+          labelSize="text-[15px] font-lg rounded-md"
+          customClasses="cursor-pointer text-white bg-[#684DF4] px-2 rounded-md transition-transform duration-300 mt-5 ease-in-out hover:scale-110 hover:opacity-90"
+        />
       </div>
     );
   }
