8d45054397fe7c797edbebcdc8466cc1ee2667e8
Sunday Stanley <sundaystanley56@gmail.com>
2025-05-19 23:23:38 +0100

    Add update

--- app/(pages)/auth/enroll/page.tsx
+++ app/(pages)/auth/enroll/page.tsx
@@ -9,11 +9,11 @@ import { IoMdArrowRoundBack } from 'react-icons/io';
 
 const EnrollPage = () => {
   return (
-    <div className="text-white w-full h-full flex flex-col p-4 md:mb-16">
+    <div className="text-white w-full h-full flex flex-col p-4 overflow-y-auto">
       <AuthTabs />
       <div className="w-full mt-2 flex flex-col md:flex-row md:items-center md:justify-between">
         <Link href="/" className="hover:text-[#684DF4] text-gray-800 flex items-center gap-2 cursor-pointer w-full">
-          <ImHome className="hidden md:block" /> <IoMdArrowRoundBack className='md:hidden block' /> <p>Back to home</p>
+          <ImHome className="hidden md:block" /> <IoMdArrowRoundBack className="md:hidden block" /> <p>Back to home</p>
         </Link>
         <TextWithLinks
           text="Already a fullSnackDev?"
@@ -23,7 +23,7 @@ const EnrollPage = () => {
           textPosition="md:justify-end font-[600] hidden md:flex"
         />
       </div>
-      <div className="flex items-center flex-col w-full h-full md:pt-10">
+      <div className="flex items-center flex-col w-full h-full md:pt-10 md:mb-10">
         <Link href="/" className="md:block hidden">
           <div className="items-center justify-center w-full flex hover:text-[#684DF4]">
             <Image src="/iconpurple.svg" alt="logo" width={500} height={500} className="h-[50px] w-[60px]" />
